a4fad89d593b92cb4f3b9f92332eef206dec7f07
UcheTheo <theo.uche2023@gmail.com>
2024-09-08 20:30:14 +0100

    Further styles to adapter to different screens

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,8 +3,6 @@ import MagicButton from './ui/MagicButton';
 import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/text-generate-effect';
 import Socials from './Socials';
-import { RiBriefcase4Fill, RiTeamFill, RiTodoFill } from 'react-icons/ri';
-import Badge from './Badge';
 import DevImg from './DevImg';
 
 type Props = {}
@@ -14,7 +12,7 @@ const words = `I bring a strong passion for technology, versatility in my skills
 const Hero = (props: Props) =>
 {
     return (
-        <div className='py-24  dark:lg:h-[820px] lg:h-[890px] xl:h-[930px] dark:xl:h-[930px] md:h-[850px] dark:md:h-[760px] dark:xm:h-[900px] xm:h-[970px] sm:h-[920px] dark:sm:h-[810px] h-[1030px] dark:h-[970px] lg:px-10 w-full bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none'>
+        <div className='py-24 dark:lg:h-[700px] lg:h-[740px] xlg:h-[750px] dark:xlg:h-[680px] lgm:h-[700px] dark:lgm:h-[650px] xl:h-[930px] dark:xl:h-[850px] md:h-[750px] dark:md:h-[670px] dark:xm:h-[700px] xm:h-[750px] sm:h-[720px] dark:sm:h-[680px] h-[780px] dark:h-[730px] lg:px-10 w-full bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none'>
             <div>
                 <Spotlight className="-top-40 -left-10
                 md:-left-32 md:-top-20 h-screen" fill="white" />
@@ -24,33 +22,23 @@ const Hero = (props: Props) =>
                 w-[50vw]" fill="blue" />
             </div>
 
-            {/* <div className="h-[84vh] pb-16 w-full dark:bg-black-100 bg-[#fef9f5]
-            dark:bg-grid-white/[0.3] bg-grid-black/[0.2] 
-            flex items-center justify-center absolute top-0 left-0 "> */}
-            {/* Radial gradient for the container to give a faded look */}
-            {/* <div className="absolute pointer-events-none 
-                inset-0 flex items-center justify-center
-                dark:bg-black-100 bg-[#fef9f5]
-                [mask-image:radial-gradient(ellipse_at_center,
-                transparent_20%,black)]" /> */}
-            {/* </div> */}
 
-            <div className="flex justify-center lg:justify-between gap-x-20  relative sm:px-14 my-20 z-10">
-                <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[55vw]
+            <div className="flex justify-center lg:justify-between gap-x-20  relative md:px-14 my-20 z-10">
+                <div className="max-w-[88vw] md:max-w-[95vw] lg:max-w-[55vw]
                 flex flex-col items-center justify-center">
                     <h2 className="uppercase tracking-widest
-                    text-md font-semibold mb-8 text-center max-w-80 text-[#FE6E58]">
+                    text-md xl:text-2xl font-semibold mb-8 text-center max-w-[22rem] text-[#FE6E58]">
                         FullStack Software Engineer
                     </h2>
 
                     <p className="h1 text-center md:tracking-wider mb-6 dark:text-white">
                         {/* <p className="text-center md:tracking-wider mb-4 
                     text-sm md:text-lg lg:text-2xl"> */}
-                        Hi, I&apos;m Theophilus, a Data Scientist
+                        Hi, I&apos;m Theophilus, a Fullstack Software Engineer
                     </p>
 
                     <TextGenerateEffect
-                        className="text-justify text-[12px] md:text-[16px] lg:text-xl leading-[20px] subtitle  mb-7 md:mb-0"
+                        className="text-justify xm:text-[110%] sm:text-[120%]  md:text-[150%] lg:text-xl xl:text-[180%] leading-[20px] subtitle  mb-7 md:mb-0"
                         words={words}
                     />
 
@@ -59,13 +47,13 @@ const Hero = (props: Props) =>
                         Hi, I&apos;m Theophilus, a Fullstack Software Engineer
                     </p> */}
 
-                    <div className="flex gap-5 mb-10">
+                    <div className="flex gap-5 mb-10 lg:w-full">
                         <a href="#about">
                             <MagicButton
                                 title="Contact Me"
                                 icon={<FaLocationArrow />}
                                 position="right"
-                                otherClasses="bg-[#FE6E58]"
+                                otherClasses="bg-[#FE6E58] hover:bg-[#D04F4A]"
                             />
                         </a>
                         <a href="#about">
@@ -73,59 +61,26 @@ const Hero = (props: Props) =>
                                 title="Download CV"
                                 icon={<FaDownload />}
                                 position="right"
-                                otherClasses="bg-black-100 border border-white"
+                                otherClasses="bg-black-100 border border-white hover:bg-[#03062B]"
                             />
                         </a>
                     </div>
 
                     {/* Socials */}
                     <Socials
-                        containerStyles="flex gap-x-6 mx-auto xl:mx-0 "
+                        containerStyles="flex w-full justify-center gap-x-6 mx-auto xl:mx-0 mt-4 md:mb-2"
                         iconStyles="text-foreground text-[22px] hover:text-[#FE6E58] transition-all"
                     />
                 </div>
 
                 <div className="hidden lg:flex relative">
-                    {/* Badge */}
-                    {/* <Badge
-                        containerStyles="absolute top-[20%] -left-[5rem]"
-                        icon={<RiBriefcase4Fill />}
-                        endCountNum={3}
-                        badgeText="Years of experience"
-                    /> */}
 
-                    {/* Badge2 */}
-                    {/* <Badge
-                        containerStyles="absolute top-[70%] -left-[1rem]"
-                        icon={<RiTeamFill />}
-                        endCountNum={6}
-                        badgeText="Happy Clients"
-                        endCountText="k"
-                    /> */}
-
-                    {/* Badge3 */}
-                    {/* <Badge
-                        containerStyles="absolute top-[63%] -right-8"
-                        icon={<RiTodoFill />}
-                        endCountNum={6}
-                        badgeText="Finished Projects"
-                        endCountText="k"
-                    /> */}
-                    {/* <div className="bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[400px] bg-no-repeat absolute -top-1 -right-2"></div> */}
                     <DevImg
-                        containerStyles=" w-[370px] h-[450px] xl:w-[500px] xl:h-[600px] relative pt-20 "
+                        containerStyles=" w-[300px] h-[350px] xlg:w-[400px] xlg:h-[450px] xl:w-[500px] xl:h-[600px] relative pt-0 "
                         imgSrc="/hero/theo2-removebg.png"
                         imgStyles="border-b relative w-[370px] h-[400px]  border-transparent ml-4 dark:border-white/[0.2] rounded-full dark:bg-black-100 bg-[#fef9f5] shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] items-center justify-center"
                     />
-                    {/* <Image
-                        src="/hero/theo2-removebg.png"
-                        width={350}
-                        height={380}
-                        alt="Picture of the author"
-                        // objectFit="cover"
-                        className="border-b rounded-full"
-                        // className="border-b relative w-[350px] h-[380px] border-transparent ml-5 dark:border-white/[0.2] rounded-full dark:bg-black-100 bg-[#fef9f5] shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] items-center justify-center"
-                    /> */}
+
                 </div>
 
             </div>
--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -17,11 +17,11 @@ const MagicButton: FC<Props> = ({
 }) =>
 {
     return (
-        <button className="relative inline-flex sm:h-[3.6rem] h-[2.7rem] w-full overflow-hidden 
-        rounded-full p-[0px] focus:outline-none md:w-60 md:mt-10">
+        <button className="relative inline-flex sm:h-[2.9rem] h-[2.7rem] w-[8.8rem] overflow-hidden 
+        rounded-full  focus:outline-none sm:w-[14rem]  xm:w-[9rem]  md:mt-10">
             <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
             <span className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full
-             sm:text-lg text-[13px] leading-[20px] font-medium text-white backdrop-blur-3xl gap-3 sm:gap-3 sm:px-4 px-2
+             sm:text-md text-[13px] leading-[20px] font-medium text-white backdrop-blur-3xl gap-3 sm:gap-3 px-2
                 ${otherClasses}`}
             >
                 {position === "left" && icon}
