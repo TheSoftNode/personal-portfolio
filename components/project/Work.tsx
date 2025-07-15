5ebdf888a3fd7e4720ec147dbd76ec6832e89a55
TheSoftNode <thesoftnode@gmail.com>
2025-07-15 13:30:22 +0100

    made the work section more responsive

--- components/project/Work.tsx
+++ components/project/Work.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-
 import Link from "next/link";
 
 // import swiper react components
@@ -18,56 +17,66 @@ import ButtonMagic from "../ui/ButtonMagic";
 import ProjectCard from "./ProjectCard";
 import { projectData } from "@/data/data";
 
-// Components
-
-
-
 type Props = {};
 
-const Work = (props: Props) =>
-{
+const Work = (props: Props) => {
   return (
-    <section className="relative  xm:w-[88%] sm:w-[75%] md:w-[85%] mx-auto mb-12 xl:mb-48 mt-12">
+    <section className="relative w-full max-w-7xl mx-auto mb-12 xl:mb-48 mt-12 px-4 sm:px-6 lg:px-8">
       <div className="container mx-auto">
-        <div className=" max-w-[100%] mx-auto xl:mx-0 text-center xl:text-left mb-12 xl:h-[400px] flex flex-col justify-center items-center xl:items-start ">
-          {/* <h2 className="section-title mb-4">Latest Projects</h2>
-          <p className="subtitle mb-8">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. ducimus !
-          </p> */}
-          <h1 className="section-title flex flex-wrap !gap-x-0  mb-8 md:mb-2">
-            <span className="mx-1">A small selection of {" "}</span>
-            <span className="dark:text-purple text-[purple]">recent projects</span>
-          </h1>
-          <Link href="/projects">
-            <ButtonMagic
-              title="View All Projects"
-              icon={<MdUnfoldMore />}
-              position="right"
-              otherClasses="!rounded-full !w-[12rem] !px-0"
-            />
-            {/* <ButtonMagic className="bg-[#FE705A]">All Projects</ButtonMagic> */}
-          </Link>
+        {/* Header Section - Improved layout for larger screens */}
+        <div className="max-w-full mx-auto xl:mx-0 text-center xl:text-left mb-12 xl:mb-16">
+          <div className="xl:max-w-[500px] flex flex-col justify-center items-center xl:items-start">
+            <h1 className="section-title flex flex-wrap !gap-x-0 mb-8 md:mb-6">
+              <span className="mx-1">A small selection of {" "}</span>
+              <span className="dark:text-purple text-[purple]">recent projects</span>
+            </h1>
+            <Link href="/projects">
+              <ButtonMagic
+                title="View All Projects"
+                icon={<MdUnfoldMore />}
+                position="right"
+                otherClasses="!rounded-full !w-[12rem] !px-0"
+              />
+            </Link>
+          </div>
         </div>
-        {/* slider */}
-        <div className="xl:max-w-[1000px] xl:absolute right-0 top-0">
+
+        {/* Slider Section - Better responsive layout */}
+        <div className="w-full xl:max-w-none">
           <Swiper
-            className="h-[800px] xs:h-[610px] xm:h-[560px] sm:h-[530px] md:h-[580px] lg:h-[600px] pb-12"
+            className="h-[800px] xs:h-[610px] xm:h-[560px] sm:h-[530px] md:h-[580px] lg:h-[600px] xl:h-[650px] 2xl:h-[700px] pb-12"
             slidesPerView={1}
             breakpoints={{
+              640: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+              },
               768: {
                 slidesPerView: 2,
+                spaceBetween: 30,
+              },
+              1024: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+              },
+              1280: {
+                slidesPerView: 3,
+                spaceBetween: 50,
+              },
+              1536: {
+                slidesPerView: 3,
+                spaceBetween: 60,
               },
             }}
-            spaceBetween={50}
+            spaceBetween={20}
             modules={[Pagination]}
             pagination={{
               clickable: true,
               el: '.swiper-custom-pagination'
             }}
           >
-            {/* show only the first 4 projects for the slides */}
-            {projectData.slice(0, 4).map((project: any, index: number) =>
-            {
+            {/* Show only the first 4 projects for the slides */}
+            {projectData.slice(0, 4).map((project: any, index: number) => {
               return (
                 <SwiperSlide key={index}>
                   <ProjectCard project={project} />
